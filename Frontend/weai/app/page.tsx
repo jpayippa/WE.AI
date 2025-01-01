90dd1bfbe39534d9de52acde1590a41cac14317c
Yoosuf25 <yoosufbakhtair@gmail.com>
2025-01-01 17:10:09 -0500

    Add AuthPage import to Home component and update trace data

--- Frontend/weai/app/page.tsx
+++ Frontend/weai/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 import { useState } from "react";
+import AuthPage from "./firebase/firebasePage"
 
 export default function Home() {
 
